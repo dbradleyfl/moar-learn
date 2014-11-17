28ba2d69fd41a9f90dc07315699e2acda1649308
Devon Bradley <dbradleyfl@gmail.com>
2014-11-17 04:47:12 -0800

    added amazon book search functionality and favicon

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 # gems
 require 'sinatra'
 require 'shotgun'
+require 'amazon/ecs'
+
+
+#config
+Amazon::Ecs.configure do |options|
+  options[:associate_tag] = 'dbradleyfl-20'
+  options[:AWS_access_key_id] = 'AKIAJSO6UUPWHVNGORKA'
+  options[:AWS_secret_key] = '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'
+end
+
 
 # routes
 
@@ -11,5 +21,14 @@ end
 get '/search' do
 	@query = params[:query]
 
-	"You want to learn about things related to #{@query}"
+	res = Amazon::Ecs.item_search( @query, :search_index => 'Books')
+
+	@query_results = []
+
+	res.items.each do |item|
+	  # retrieve string value using XML path
+	  @query_results << {title: item.get('ItemAttributes/Title'), url: item.get('DetailPageURL')}
+	end
+
+	erb :results
 end
