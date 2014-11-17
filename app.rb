a656e8deb8c0df101de1f4112a7c766f40b96f85
Devon Bradley <dbradleyfl@gmail.com>
2014-11-17 13:54:26 -0800

    extracted amazon api to a model class called AmazonAPI

--- app.rb
+++ app.rb
@@ -7,16 +7,27 @@ require 'Unirest'
 
 require_relative 'keys.rb'
 
-#config
-Amazon::Ecs.configure do |options|
-  options[:associate_tag] = AMAZON_TRACKING_ID
-  options[:AWS_access_key_id] = AMAZON_ACCESS_KEY_CODE
-  options[:AWS_secret_key] = AMAZON_SECRET_KEY_CODE
+#Models
+
+class AmazonAPI
+	Amazon::Ecs.configure do |options|
+	  options[:associate_tag] = AMAZON_TRACKING_ID
+	  options[:AWS_access_key_id] = AMAZON_ACCESS_KEY_CODE
+	  options[:AWS_secret_key] = AMAZON_SECRET_KEY_CODE
+	end
+
+	def self.getBooks(topic)
+		query_results = []
+		res = Amazon::Ecs.item_search( topic, :search_index => 'Books')
+
+		res.items.each do |item|
+		  # retrieve string value using XML path
+		query_results << {title: item.get('ItemAttributes/Title'), url: item.get('DetailPageURL')}
+		end
+
+		return query_results
+	end
 end
-
-
-
-
 
 
 # controllers / routes
@@ -28,14 +39,7 @@ end
 get '/search' do
 	@query = params[:query]
 
-	res = Amazon::Ecs.item_search( @query, :search_index => 'Books')
-
-	@query_results = []
-
-	res.items.each do |item|
-	  # retrieve string value using XML path
-	  @query_results << {title: item.get('ItemAttributes/Title'), url: item.get('DetailPageURL')}
-	end
+	@query_results = AmazonAPI.getBooks(@query)
 
 	erb :results
 end
